901fa7642a48e3e0ef9bc08f0ea59990d2bd8619
Carmela <carmelalaparedes@gmail.com>
2017-07-19 13:27:35 +0800

    Created Pages controller, Added home, index, and profile pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  #root 'pages#index'
+  get 'pages/index' => 'pages#index'
+  get 'pages/home' => 'pages#home'
+  get 'pages/profile' => 'pages#profile'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
